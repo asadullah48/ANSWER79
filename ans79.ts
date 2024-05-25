33ca0fa96d7b92a490dd381659640471c8785859
asadullah48 <asadullahshafique@hotmail.com>
2024-05-25 11:34:13 +0500

    Initial commit

---
+++ ans79.ts
@@ -0,0 +1,11 @@
+// Q 79
+// This sets up an object for a car with specific details
+let car = {
+    make: "Toyota",
+    model: "Cross",
+    year: 2022,
+  };
+  
+  // Accessing and showing the car's model
+  console.log(car.model); // Outputs: Corolla
+  // We use dot notation (car.model) to get the model of the car from our object.
